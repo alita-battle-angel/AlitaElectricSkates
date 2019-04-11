fc3b88dc02f5573c74a5726f50997fb6d57bcce3
Alexander <nixon2992@gmail.com>
2019-04-11 23:18:47 +0300

    add  fault  irq module

---
+++ src/ErrorProvider.c
@@ -0,0 +1,20 @@
+#include "ErrorProvider.h"
+
+void HardFault_Handler(void)
+{
+}
+
+void    MemManage_Handler (void)
+{
+  
+}
+
+void    BusFault_Handler(void)
+{
+  
+}
+
+void UsageFault_Handler (void)
+{
+    
+}
---
+++ src/ErrorProvider.h
@@ -0,0 +1,13 @@
+#ifndef _ERROR_PROVIDER_
+#define _ERROR_PROVIDER_
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+void HardFault_Handler(void);
+void MemManage_Handler (void);
+void BusFault_Handler(void);
+void UsageFault_Handler (void);
+#ifdef __cplusplus
+ }
+#endif
+#endif
