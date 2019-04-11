7339a7e3c09e7fff9e47a493de1a7b7e044ecfdd
Alexander <nixon2992@gmail.com>
2019-04-12 00:02:58 +0300

    fix sequnce in main to start LED test

--- src/main.c
+++ src/main.c
@@ -8,12 +8,12 @@ int main()
   Clock_HSI_Init();
   Clock_HSE_Init();
   LED_Init();
+  Timer_WaitInit();
   LED_ModuleTest();
   if(iWD_Fault_is_Detected())
   {
      LED_On(LED_RED);
   }
-  Timer_WaitInit();
   Button_ModuleTest();
   iWD_Init();
   while(1)
