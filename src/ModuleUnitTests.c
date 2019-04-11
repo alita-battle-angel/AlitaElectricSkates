3daf74ca145d1c111a83bca653c70791f07db1ff
Alexander <nixon2992@gmail.com>
2019-04-11 23:21:11 +0300

    add module unit tests

---
+++ src/ModuleUnitTests.c
@@ -0,0 +1,84 @@
+#include "ModuleUnitTests.h"
+#include "LED.h"
+#include "Button.h"
+#include "Timer.h"
+Button_InitStructure ButtonControl;
+const LED leds[] = { LED_GREEN, LED_RED, LED_ORANGE, LED_BLUE };
+
+void LED_ModuleTest(void)
+{
+  int all_leds = 0;
+  for (uint8_t i = 0; i < sizeof(leds) / sizeof(leds[0]); i++) {
+    all_leds |= leds[i];
+  }
+  for (uint8_t i = 0; i < 3; i++) {
+    LED_On((LED)all_leds);
+    Timer_WaitMilisec(75);
+    LED_Off((LED)all_leds);
+    Timer_WaitMilisec(100);
+  } 
+}
+
+void LED_ModuleTestLoop(void)
+{
+   for (uint8_t i = 0; i < sizeof(leds) / sizeof(leds[0]); i++) {
+      LED led = leds[i];
+      LED_On(led);
+      Timer_WaitMilisec(100);
+      LED_Off(led);
+    }
+}
+
+void Button_ModuleTestLoop(void)
+{
+   Button_Check(&ButtonControl);  
+}
+
+static void Button_StateChanged(Button_State state)
+{
+  switch(state)
+  {
+    case Button_State_Pressed:
+    {
+      LED_On(LED_GREEN);
+      break;
+    }
+    case Button_State_NotPressed:
+    {
+      LED_Off(LED_GREEN);
+      break;  
+    }
+  } 
+}
+
+static void Button_SignalChanged(Button_SignalEdge state)
+{
+  switch(state)
+  {
+    case Button_SignalEdge_Rissing:
+    {
+      LED_On(LED_RED);
+      break;   
+    }
+    case  Button_SignalEdge_Falling:
+    {
+      LED_Off(LED_RED);
+      break;  
+    }
+  
+  }
+}
+
+void Button_ModuleTest(void)
+{
+  
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  ButtonControl.Pin                  = GPIO_Pin_0;
+  ButtonControl.Port                 = GPIOA;
+  ButtonControl.OnEdgeStateChanged   = Button_SignalChanged;
+  ButtonControl.OnButtonStateChanged = Button_StateChanged;
+  ButtonControl.StableStateThershold = 5;
+  Button_Init(&ButtonControl);
+  
+}
+
---
+++ src/ModuleUnitTests.h
@@ -0,0 +1,13 @@
+#ifndef _ModuleUniTest_
+#define _ModuleUniTest_
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+void LED_ModuleTest(void);
+void LED_ModuleTestLoop(void);
+void Button_ModuleTest(void);
+void Button_ModuleTestLoop(void);
+#ifdef __cplusplus
+}
+#endif 
+#endif
